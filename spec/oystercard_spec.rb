093e31180c8248d4efc5bd93c48243af6bd69c13
Prashantraniga <prashant.raniga2012@gmail.com>
2018-10-08 15:32:54 +0100

    Chapter#5 - refactored

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -14,15 +14,17 @@ it 'has a balance of 0' do
   expect(subject.balance).to eq 0
 end
 
+describe '#top_up' do
+
+  it { is_expected.to respond_to(:top_up).with(1).argument }
+
+
 it 'can be topped up' do
 
-  expect(subject).to respond_to(:top_up).with(1).argument
+  expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
 end
 
-it 'increases balance of card' do
-  card = Oystercard.new
-  card.top_up(20)
-  expect(card.balance).to eq 20
 end
+
 
 end
